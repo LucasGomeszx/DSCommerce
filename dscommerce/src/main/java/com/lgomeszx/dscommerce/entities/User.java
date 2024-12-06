024af807096af267e202a2a317de6ed383bc0be0
DESKTOP-1V4O4KU\LucasGomes <lgomeszx@gmail.com>
2024-12-05 23:28:52 -0300

    Adicionando projeto

---
+++ dscommerce/src/main/java/com/lgomeszx/dscommerce/entities/User.java
@@ -0,0 +1,12 @@
+package com.lgomeszx.dscommerce.entities;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+}
