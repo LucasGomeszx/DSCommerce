5ec1635f1b3d5e03411a9fc2fe4628210d85489a
DESKTOP-1V4O4KU\LucasGomes <lgomeszx@gmail.com>
2025-01-13 23:26:39 -0300

    finalizando modulo 2

--- dscommerce/src/main/java/com/lgomeszx/dscommerce/entities/Order.java
+++ dscommerce/src/main/java/com/lgomeszx/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.lgomeszx.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -14,6 +17,7 @@ public class Order {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
+
     private OrderStatus status;
 
     @ManyToOne
@@ -22,6 +26,13 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Order() {
+
+    }
 
     public Order(long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
@@ -69,6 +80,27 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProduct() {
+        return  items.stream().map(x -> x.getProduct()).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return id == order.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
     }
 
 }
--- dscommerce/src/main/java/com/lgomeszx/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/lgomeszx/dscommerce/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +17,7 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String phone;
@@ -90,6 +92,19 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 
 }
